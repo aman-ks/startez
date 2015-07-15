8c4048d23ed82c40e9062b850d93417bd733f981
Aman K Srivastava <eamanshrivastava@gmail.com>
2015-07-15 17:05:28 +0530

    Preferred Feed endpoint done.

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def ratings(user_id, element):
             data = {'status':'rated','all_rating':rating_list}
             return jsonify(data)    
 
-@app.route("/feed", methods= ['POST','GET'])
+@app.route("/feed")
 def feed():
 
     last_id = app.redis.get('last_user')
@@ -64,6 +64,35 @@ def feed():
     else:
         return 'Not found'        
 
+@app.route("/pref-feed", methods=['POST'])
+def nfeed():
+
+    sector = request.json.get('sector')
+    location = request.json.get('location')
+    stage = request.json.get('stage')
+    last_id = app.redis.get('last_user')
+    last_id = int(last_id)
+
+    if sector and stage and location:
+        retrieve = ['user:'+str(x) for x in range(1000,last_id)]
+        data = {}
+
+        for user in retrieve:
+            s = app.redis.hget(user,'sector')
+            l = app.redis.hget(user,'location')
+            st = app.redis.hget(user,'stage')
+            
+            if s==sector and l==location and st==stage:
+                key_user_id = app.redis.hget(user,'id')
+                value_user_data = app.redis.hgetall(user)
+                data[key_user_id] = value_user_data 
+
+        return jsonify({'data':data})
+
+    else:
+        return jsonify({'status':'not found','text':'Could not find the respective filters.'})    
+
+# @app.route("/investors")
     
 if __name__ == "__main__":
     port = 5000
